bf018cf22a496a3117e99fffb05f5ba7dac5c282
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-09-04 11:45:05 +0600

    Final touch

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const loadCategory = async() =>{
         const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    displayCategory(data.data.news_category);
+    return displayCategory(data.data.news_category);
     }
     catch{
         console.log(error);
@@ -17,11 +17,10 @@ const displayCategory = catagories =>{
     for(const category of catagories){
         // console.log(category);
     const categoryTray = document.getElementById('category-tray');
-    const { category_id, category_name } = category;
     const categoryDiv = document.createElement('div');
     categoryDiv.classList.add('col');
     categoryDiv.innerHTML = `
-    <a href="#" onclick="loadCategoryNews('${category_id}')" class="btn btn-outline-warning">${category_name}</a>
+    <a href="#" onclick="loadCategoryNews('${category.category_id}')" class="btn btn-outline-warning">${category.category_name}</a>
     `;
     categoryTray.appendChild(categoryDiv);
     }
@@ -29,20 +28,16 @@ const displayCategory = catagories =>{
 
 
 // Load Catagory news 
-const loadCategoryNews = async(id)=>{
-        try{
-            const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
-            const res = await fetch(url);
-            const data = await res.json();
-            displayCategoryNews(data.data);
-        }
-        catch{
-            console.log(error);
-        }
+const loadCategoryNews = (id)=>{
+        const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategoryNews(data.data))
 }
 
 // Display category News 
-const displayCategoryNews = async news =>{
+const displayCategoryNews = allNews =>{
+    allNews.forEach(news =>{
         console.log(news);
         const categoryNewsTray = document.getElementById('news-tray');
         categoryNewsTray.textContent = '';
@@ -54,6 +49,7 @@ const displayCategoryNews = async news =>{
         <div class="card-body">
         <h5 class="card-title">${news.title ? news.title : 'No Title Found'}</h5>
         <p class="card-text">${news.details ? news.details.slice(0,180) : '<span class="text-warning">No Detail found</span>'}...</p>
+
         <div class="row align-items-center">
         <div class="col-4 d-flex">
         <img class="w-25 h-50 rounded-circle" src="${news.author.img ? news.author.img : '<span class="text-warning">No Author found</span>'}">
@@ -109,9 +105,12 @@ const modalContainer = document.getElementById('exampleModal');
           </div>
         </div>
     `;
-
+    });
 }
 
+document.getElementById('hot-news').addEventListener('click', function(){
+    document.getElementById('blog-article').classList.remove('d-none');
+})
 
 
 loadCategory();
